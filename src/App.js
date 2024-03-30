9c9ef7d1c4568c1a0207d9695a949038c7eb9d6b
sundaybamidele <158308923+sundaybamidele@users.noreply.github.com>
2024-03-30 17:44:07 +0000

    UPDATE

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from'react';
 import Home from './Pages/Home.js';
 import { Link } from 'react-router-dom';
+import MyRouter from './router/index.js';
 
 function App() {
   return (
     <div>
-     <Link to="/"> Home</link> 
+     <Link to="/"> Home</Link> 
      <Link to="/about"> About Us</Link>  
      <Link to="/contact"> Contact Us</Link>  
-     <Home /> 
+     <Home />
 
-
+     <MyRouter />
       
     </div>
   );
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,18 @@
 
-import { BrowserRouter, Route } from'react-router-dom'; 
+import {Routers, Route } from'react-router-dom'; 
 import Home from '../Pages/Home.js';
 import About from '../Pages/About.js';
 import Contact from '../Pages/Contact.js';
 
+
 function MyRouter(){
 
-    return (
-        <BrowserRouter>  
-                  
-                <Route path="/" component={Home} />
+    return (        
+        <Routers>
+                <Route path="/" component={<Home />} />
                 <Route path="/about-us" component={About} />
                 <Route path="/contact-us" component={Contact} />
-            
-        </BrowserRouter>
-        
-
+        </Routers> 
     )               
 }
 export default MyRouter;
