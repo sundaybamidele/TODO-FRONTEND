6ca26a757a65e399788271b67ba1811286e052c2
sundaybamidele <158308923+sundaybamidele@users.noreply.github.com>
2024-03-30 21:05:25 +0000

    Update

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -14,6 +14,23 @@ return (
                         <Link to="/"> Add New Task</Link>
                     </h4>
                 </div>
+                <div className="card-body">
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Description</th>
+                                <th>Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
