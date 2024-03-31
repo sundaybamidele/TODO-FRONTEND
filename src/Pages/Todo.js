b6dc6a75a864cd544e430d8e0cb0c5c2d2465afc
sundaybamidele <158308923+sundaybamidele@users.noreply.github.com>
2024-03-31 01:00:19 +0000

    update

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,54 +1,68 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+function Todo() {
+  const [todo, setTodo] = useState([]);
 
-
-function Todo(){    
-
-useEffect(() => {
-    axios.get('http://localhost:8080/api/todos').then((response) => {
-        console.log(response);
-   
-       
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/todo").then((response) => {
+      console.log(response);
+      setTodo(response.data.todo);
     });
+  }, []);
+  var todoDetails = "";
+  todoDetails = todo.map((item, index) => {
+    return (
+      <tr key={index}>
+        <td>{item.id}</td>
+        <td>{item.title}</td>
+        <td>{item.description}</td>
+        <td>{item.status}</td>
+        <td>
+          <Link to={`/todo/${item.id}`} className="btn btn-success">
+            Edit
+          </Link>
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    );
+  });
 
-   
-}, []);
-
-return (
-<div className="container mt-5">
-    <div className="row">
+  return (
+    <div className="container mt-5">
+      <div className="row">
         <div className="col-md-12">
-            <div className="card">
-                <div className="card-header">
-                    <h4>Todo List
-                        <Link to="/" className="btn btn-primary float-end"> Add New Task</Link>
-                    </h4>
-                </div>
-                <div className="card-body">
-                    <table className="table table-striped" border={10} bgcolor="#f1f1f1">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Status</th>
-                                <th>Edit</th>
-                                <th>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                        </tbody>
-                    </table>
-                </div>
+          <div className="card">
+            <div className="card-header">
+              <h4>
+                Todo List
+                <Link to="/" className="btn btn-primary float-end">Add New Task</Link>
+              </h4>
             </div>
+            <div className="card-body">
+              <table className="table table-striped" border={3}>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>{todoDetails}</tbody>
+              </table>
+            </div>
+          </div>
         </div>
+      </div>
+      <h1>This is Todo Page</h1>
     </div>
-<h1>This is Todo Page</h1>
-</div>
-)
+  );
 }
 
 export default Todo;
