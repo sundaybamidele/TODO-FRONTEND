d34ab61ffd7a8a29ae33fc602521f945ff2819e0
sundaybamidele <158308923+sundaybamidele@users.noreply.github.com>
2024-04-02 21:16:04 +0000

    Delete function implemented successfully

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import Pageloading from "../components/Pageloading";
 
-
 function Todo() {
-
-  
   const [loading, setLoading] = useState(true);
-  
-  const [todos, setTodo] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    axios.get("http://127.0.0.1:8000/api/todo").then((res) => {
-      console.log(res);
-      setTodo(res.data.todos);
-      setLoading(false)
-    });
+    axios.get("http://127.0.0.1:8000/api/todo")
+      .then((res) => {
+        console.log(res);
+        setTodos(res.data.todos);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+        setLoading(false);
+      });
   }, []);
-  
-  var todoDetails = "";
-  todoDetails = todos.map((item, index) => {
-    return (
-      <tr key={index}>
-        <td>{item.id}</td>
-        <td>{item.title}</td>
-        <td>{item.description}</td>
-        <td>{item.status}</td>
-        <td>
-          <Link to={`/todo/${item.id}/edit`} className="btn btn-success">
-            Edit
-          </Link>
-        </td>
-        <td>
-          <button className="btn btn-danger">Delete</button>
-        </td>
-      </tr>
-    );
-  });
+
+  const deleteTodo = (id) => {
+    const confirmed = window.confirm("Are you sure you want to delete this record?");
+    if (!confirmed) return; // Cancel deletion if user cancels the confirmation
+
+    axios.delete(`http://127.0.0.1:8000/api/todo/${id}/delete`)
+      .then((res) => {
+        alert(res.data.message);
+        // Filter out the deleted todo item from the todos array
+        setTodos(todos.filter(todo => todo.id !== id));
+      })
+      .catch((error) => {
+        console.error("Error deleting todo:", error);
+        if (error.response) {
+          if (error.response.status === 404) {
+            alert(error.response.data.message);
+          } else if (error.response.status === 500) {
+            alert(error.response.data);
+          }
+        }
+      });
+  };
 
   if (loading) {
-    return(
-      <Pageloading/>
-    )
+    return <Pageloading />;
   }
+
   return (
     <div className="container mt-5">
       <div className="row">
@@ -58,7 +60,7 @@ function Todo() {
               </h4>
             </div>
             <div className="card-body">
-              <table className="table table-striped" border={3}>
+              <table className="table table-striped">
                 <thead>
                   <tr>
                     <th>ID</th>
@@ -69,13 +71,31 @@ function Todo() {
                     <th>Delete</th>
                   </tr>
                 </thead>
-                <tbody>{todoDetails}</tbody>
+                <tbody>
+                  {todos.map((todo, index) => (
+                    <tr key={index}>
+                      <td>{todo.id}</td>
+                      <td>{todo.title}</td>
+                      <td>{todo.description}</td>
+                      <td>{todo.status}</td>
+                      <td>
+                        <Link to={`/todo/${todo.id}/edit`} className="btn btn-success">
+                          Edit
+                        </Link>
+                      </td>
+                      <td>
+                        <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
             </div>
           </div>
         </div>
       </div>
-      <h1>This is Todo Page</h1>
     </div>
   );
 }
