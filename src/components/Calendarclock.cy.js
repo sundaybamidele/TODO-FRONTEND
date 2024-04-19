c2135c7b0844d8356debc7ef959276092011aaa3
sundaybamidele <158308923+sundaybamidele@users.noreply.github.com>
2024-04-19 15:39:32 +0000

    Cypress working now

--- src/components/Calendarclock.cy.js
+++ src/components/Calendarclock.cy.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { mount } from '@cypress/react'; // Import mount from @cypress/react
+import { BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Calendarclock from './Calendarclock';
 
-describe('<Calendarclock />', () => {
+describe('<Navbar />', () => {
   it('renders', () => {
-    // Mount the Calendarclock component
-    mount(<Calendarclock />);
-
-    // Verify that the component is rendered
-    cy.get('.react-calendar').should('be.visible'); // Assuming the calendar component has the class 'react-calendar'
-    cy.get('.react-clock').should('be.visible'); // Assuming the clock component has the class 'react-clock'
-  });
-
-  it('selects a date', () => {
-    // Mount the Calendarclock component
-    mount(<Calendarclock />);
-
-    // Simulate selecting a date
-    cy.get('.react-calendar__tile--now').click(); // Click on today's date
-
-    // Assert that the selected date is displayed in the calendar input
-    cy.get('.react-calendar__tile--active').contains('Today');
-  });
-});
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(
+      <BrowserRouter>
+     <Calendarclock />
+    </BrowserRouter>
+    )
+  })
+})
